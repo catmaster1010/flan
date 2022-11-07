b146436b44ff4b33fe89315863f1263cd920e863
Nich9e <66039026+nichoke@users.noreply.github.com>
2022-11-07 14:58:43 -0500

    Update loadgdt.asm

--- src/memmory/gdt/loadgdt.asm
+++ src/memmory/gdt/loadgdt.asm
@@ -10,13 +10,13 @@ load_gdt:
         ret
 reloadSegments:
         ; Reload CS register:
-        PUSH 0x28                 ; Push code segment to stack, 0x08 is a stand-in for your code segment
+        PUSH 0x28                 ; Push code segment to stack
         LEA RAX, [rel .reload_CS] ; Load address of .reload_CS into RAX
         PUSH RAX                  ; Push this value to the stack
-        RETFQ                     ; Perform a far return, RETFQ or LRETQ depending on syntax
+        RETFQ                     ; Perform a far return
 .reload_CS:
         ; Reload data segment registers
-        MOV   AX, 0x30 ; 0x10 is a stand-in for your data segment
+        MOV   AX, 0x30 ; data segment
         MOV   DS, AX
         MOV   ES, AX
         MOV   FS, AX
