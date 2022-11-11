6507250896a62e24f3f9ea959e9f001026e12a7a
nichoke <dximixi@gmail.com>
2022-11-11 11:40:28 -0500

    rewrite!

--- src/interrupts/idt/idt.c
+++ src/interrupts/idt/idt.c
@@ -2,20 +2,11 @@
 #include "std/stdio.h"
 #include <stdint.h>
 
-static idt_gate_t encodeIdtEntry(uint64_t offset, uint8_t type)
-{
-    return (idt_gate_t){
-        .lowOffset16 = offset & 0xFFFF,
-        .selector16 = 0x28,
-        .ist8 = 0,
-        .typeAttributes8 = type,
-        .midOffset16 = (offset >> 16) & 0xFFFF,
-        .highOffset32 = (offset >> 32) & 0xFFFFFFFF,
-        .reserved = 0};
-}
+
+static idt_gate_t idt[256];
 
 void idt_load() {
-    static idt_gate_t idt[256];
+
 
     static idt_register_t idtr;
     idtr.limit = (sizeof(idt) - 1);
@@ -23,13 +14,37 @@ void idt_load() {
 
     extern void load_gdt(uint16_t limit, uint64_t base);
     load_gdt(idtr.limit, idtr.base);
+
+
+
     printf("IDT Loaded.");
 }
+
+
+
 void isr_init(){}
 
 
 void idt_init(){
     idt_load();
     isr_init();}
+/////////////////////////
+
+void exception_handler(){
+    printf("EXCEPTION RECIVED!!");
+    __asm__ volatile ("cli; hlt");
+}
+
+static void encode_idt_entry(uint8_t vector, void* isr, uint8_t flags) {
+    idt_gate_t* descriptor = &idt[vector];
+ 
+    descriptor->lowOffset16       = (uint64_t)isr & 0xFFFF;
+    descriptor->selector16        = 0x28; //Kernel code offset. 
+    descriptor->ist8              = 0; //not using Interrupt Stack Table. 
+    descriptor->typeAttributes8   = flags; //gate type, dpl, p (must be set to 1).
+    descriptor->midOffset16       = ((uint64_t)isr >> 16) & 0xFFFF;
+    descriptor->highOffset32      = ((uint64_t)isr >> 32) & 0xFFFFFFFF;
+    descriptor->reserved32           = 0;
+}
 
 
--- src/interrupts/idt/idt.h
+++ src/interrupts/idt/idt.h
@@ -14,11 +14,11 @@ typedef struct idt_register idt_register_t;
 struct idt_gate {
    uint16_t lowOffset16;        // offset bits 0..15
    uint16_t selector16;        // a code segment selector in GDT or LDT
-   uint8_t  ist8;             // bits 0..2 holds Interrupt Stack Table offset, rest of bits zero.
+   uint8_t  ist8;             // bits 0..2 holds Interrupt Stack Table offset, rest of bits zero (RESERVED).
    uint8_t  typeAttributes8; // gate type, dpl, and p fields
    uint16_t midOffset16;        // offset bits 16..31
    uint32_t highOffset32;        // offset bits 32..63
-   uint32_t reserved;            // reserved
+   uint32_t reserved32;            // reserved (aka 0)
 }__attribute__((packed));
 typedef struct idt_gate idt_gate_t; 
 
