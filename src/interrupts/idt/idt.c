4c1c9cdd9f0c4f965e41f88a3bcf5594bbf55401
nichoke <dximixi@gmail.com>
2022-11-11 13:48:05 -0500

    Nearly done ISR.

--- src/interrupts/idt/idt.c
+++ src/interrupts/idt/idt.c
@@ -5,6 +5,20 @@
 
 static idt_gate_t idt[256];
 
+static void encode_idt_entry(uint8_t vector, void* isr, uint8_t flags) {
+    idt_gate_t* descriptor = &idt[vector];
+ 
+    descriptor->lowOffset16       = (uint64_t)isr & 0xFFFF;
+    descriptor->selector16        = 0x28; //Kernel code offset. 
+    descriptor->ist8              = 0; //not using Interrupt Stack Table. (this is null sector + ist)
+    descriptor->typeAttributes8   = flags; //gate type, dpl, p (must be set to 1).
+    descriptor->midOffset16       = ((uint64_t)isr >> 16) & 0xFFFF;
+    descriptor->highOffset32      = ((uint64_t)isr >> 32) & 0xFFFFFFFF;
+    descriptor->reserved32        = 0; //reserved for the upper bit of 64bit gdt. Should always be set to 0.
+}
+
+
+
 void idt_load() {
 
 
@@ -22,29 +36,28 @@ void idt_load() {
 
 
 
-void isr_init(){}
+void isr_init(){
+    /*extern void isr_stub_table;
+    for (uint8_t vector = 0; vector < 32; vector++) {
+        encode_idt_entry(vector, &isr_stub_table[vector], 0x8E);//0b10001110 hardware interupts ignore this. (which is this since its the first 32 entries)
+
+        //0b1110 or 0xE: 64-bit Interrupt Gate
+        //0b1111 or 0xF: 64-bit Trap Gate
+    
+    }*/
+
+}
 
 
 void idt_init(){
+    isr_init();
     idt_load();
-    isr_init();}
+}
 /////////////////////////
 
 void exception_handler(){
     printf("EXCEPTION RECIVED!!");
-    __asm__ volatile ("cli; hlt");
-}
-
-static void encode_idt_entry(uint8_t vector, void* isr, uint8_t flags) {
-    idt_gate_t* descriptor = &idt[vector];
- 
-    descriptor->lowOffset16       = (uint64_t)isr & 0xFFFF;
-    descriptor->selector16        = 0x28; //Kernel code offset. 
-    descriptor->ist8              = 0; //not using Interrupt Stack Table. 
-    descriptor->typeAttributes8   = flags; //gate type, dpl, p (must be set to 1).
-    descriptor->midOffset16       = ((uint64_t)isr >> 16) & 0xFFFF;
-    descriptor->highOffset32      = ((uint64_t)isr >> 32) & 0xFFFFFFFF;
-    descriptor->reserved32           = 0;
-}
+    __asm__ volatile ("cli; hlt"); //Change this latter. 
+} 
 
 
--- src/interrupts/idt/loadidt.asm
+++ src/interrupts/idt/loadidt.asm
@@ -1,5 +1,8 @@
 section	.text
-	    global load_idt
+        extern exception_handler
+	global load_idt
+        
+        global isr_stub_table
 load_idt:
         cli
         MOV   [idtr], DI
@@ -8,7 +11,62 @@ load_idt:
         sti ; Enable interrupts again.
         ret
 
+;IRETQ (Return from interrupts) POPS RIP AND CS (RIP:CS)
 
+%macro isr_err_stub 1
+isr_stub_%+%1:
+    call exception_handler ;Located in IDT.c
+    iretq ;Preform long jump.
+%endmacro
+
+%macro isr_no_err_stub 1
+isr_stub_%+%1: 
+    call exception_handler ;Located in IDT.c
+    iretq ;Preform long jump. 
+%endmacro
+
+
+extern exception_handler
+isr_no_err_stub 0
+isr_no_err_stub 1
+isr_no_err_stub 2
+isr_no_err_stub 3
+isr_no_err_stub 4
+isr_no_err_stub 5
+isr_no_err_stub 6
+isr_no_err_stub 7
+isr_err_stub    8
+isr_no_err_stub 9
+isr_err_stub    10
+isr_err_stub    11
+isr_err_stub    12
+isr_err_stub    13
+isr_err_stub    14
+isr_no_err_stub 15
+isr_no_err_stub 16
+isr_err_stub    17
+isr_no_err_stub 18
+isr_no_err_stub 19
+isr_no_err_stub 20
+isr_no_err_stub 21
+isr_no_err_stub 22
+isr_no_err_stub 23
+isr_no_err_stub 24
+isr_no_err_stub 25
+isr_no_err_stub 26
+isr_no_err_stub 27
+isr_no_err_stub 28
+isr_no_err_stub 29
+isr_err_stub    30
+isr_no_err_stub 31
+
+;Stub table which prevents execssive code use
+isr_stub_table:
+%assign i 0 
+%rep    32 
+    DQ isr_stub_%+i 
+%assign i i+1 
+%endrep
 
 section	.data
 idtr DW 0
