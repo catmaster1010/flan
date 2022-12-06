21f4a263ac903c35dcf229165923e98140df924a
nichoke <nichoke@github.com>
2022-12-05 20:57:02 -0500

    Buggy

--- src/memmory/pmm.c
+++ src/memmory/pmm.c
@@ -3,55 +3,103 @@
 #include <limine.h>
 #include "std/stddef.h"
 
-#define ALIGN_UP(num,align) (((num) + align - 1) & ~(align - 1))
-#define ALIGN_DOWN(num,align) ((num) & ~(align - 1))
-
-
-#define container_of(ptr, type, member) ({                      \
-        const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
-        (type *)( (char *)__mptr - offsetof(type,member) );})
-
-static dll_t* free_list;
+#define HEADER_SIZE 24
+static dll_t free_list;
 
 volatile struct limine_memmap_request memmap_request = {
     .id = LIMINE_MEMMAP_REQUEST,
     .revision = 0
     };
 
+
+void dll_list_add(dll_t* n, dll_t* prev, dll_t* next){
+	next->prev = n;
+	n->next = next;
+	n->prev = prev;
+	prev->next = n;
+}
+
+
+
 void pmm_init()
 {
     printf("There are %d entries in the mmap\n", memmap_request.response->entry_count);
     for (int i = 0; i < memmap_request.response->entry_count; i++)
     {
-        printf("entry %d    base: %x    length: %x    type: %d    tail: %x\n",
-               i+1,
-               memmap_request.response->entries[i]->base,
-               memmap_request.response->entries[i]->length,
-               memmap_request.response->entries[i]->type,
-               memmap_request.response->entries[i]->base+memmap_request.response->entries[i]->length
-               );
         if (memmap_request.response->entries[i]->type ==0)
         {
-       //    add_block(memmap_request.response->entries[i]->base,memmap_request.response->entries[i]->length);
+            if (!free_list.next)
+            {
+                alloc_node_t *block = (uint64_t *) ALIGN_UP((uint64_t)memmap_request.response->entries[i]->base, 8);
+                 //actual size - alignment-headerspace 
+                block->size = memmap_request.response->entries[i]->base + memmap_request.response->entries[i]->length - (uint64_t)block - HEADER_SIZE;
+                free_list.next=&block->node;
+                block->node.prev=&free_list;
+                printf("%d made the first block in freelist.\n",i+1);//--
+            }
+            else{
+                add_block(memmap_request.response->entries[i]->base,memmap_request.response->entries[i]->length);
+                printf("%d another block in freelist. \n",i+1); //--
+            }   
         }
+        printf("entry %d    base: %d    length: %x    type: %d    tail: %x\n",
+                i+1,
+                memmap_request.response->entries[i]->base,
+                memmap_request.response->entries[i]->length,
+                memmap_request.response->entries[i]->type,
+                memmap_request.response->entries[i]->base+memmap_request.response->entries[i]->length 
+            );
     }   
+    for (int i = 0; i < 3; i++)
+    {
+        int *a=malloc(4);
+        printf("Adress of malloc: %d\n\n",a);
+    }
 }
 
 void add_block(uint64_t *addr, uint64_t size){
     alloc_node_t *block = (uint64_t *) ALIGN_UP((uint64_t)addr, 8);
     //actual size - alignment-headerspace 
-    block->size = addr + size - (uint64_t)block - 24;
+    block->size = addr + size - (uint64_t)block - HEADER_SIZE;
     
-    free_list->next->prev = &block->node;
-	block->node.next = free_list->next;
-	block->node.prev = free_list;
-	free_list->next = &block->node.next;
+    dll_list_add(&block->node,&free_list,&free_list.next);
 };
 
 
 
-void malloc(uint64_t size){
-    dll_t* temp = &free_list;
+uint64_t* malloc(uint64_t size){
+    void * ptr;
+    alloc_node_t *block;
+
+    // Try to find a big enough block to alloc (First fit)
+  for (block = container_of(free_list.next,alloc_node_t,node); &block->node != &free_list; block=container_of(block->node.next,alloc_node_t,node))
+    {      
+        if (block->size>=size)
+        {   
+            ptr = &block->cBlock;
+            printf("Found block for requested size.\n");
+            break;
+        }        
+    }
+
+    if (!ptr){printf("Could not find block for requested size."); return NULL;}
+    //Can block be split
+    if((block->size - size) >= HEADER_SIZE)
+        {
+            alloc_node_t *new_block = (alloc_node_t *)((&block->cBlock) + size);
+            new_block->size = block->size - (size - HEADER_SIZE);
+            block->size = size;
+            //add new block to list
+            dll_list_add(&new_block->node,&block->node,block->node.next);
+        }
+    //remove block from list since its getting allocated 
+    block->node.next->prev =block->node.prev;
+    block->node.prev->next=block->node.next;
+    block->node.next=NULL;
+    block->node.prev=NULL;
+    //Finally, return pointer to newly allocated adress
+    return ptr;
     
- 
 }
+
+void free(){}
--- src/memmory/pmm.h
+++ src/memmory/pmm.h
@@ -1,8 +1,6 @@
 #ifndef pmm_h
 #define pmm_h
 
-void pmm_init();
-
 struct dll {
      struct dll *next;
      struct dll *prev;
@@ -12,12 +10,26 @@ typedef struct dll dll_t;
 struct alloc_node {
      dll_t node;
      uint64_t size;
+     char * cBlock;
 }__attribute__((packed));
 typedef struct alloc_node alloc_node_t;
 
 void dll_add( dll_t* n,  dll_t* head);
 void add_block(uint64_t *addr, uint64_t size);
-void malloc(uint64_t size);
+uint64_t* malloc(uint64_t size);
 
+void pmm_init();
+void free();
 
+void dll_list_add(dll_t* n, dll_t* prev, dll_t* next);
+
+#define ALIGN_UP(num,align) (((num) + align - 1) & ~(align - 1)) 
+#define ALIGN_DOWN(num,align) ((num) & ~(align - 1))
+#define container_of(ptr, type, member) ( \
+        (type *)( (char *)ptr - offsetof(type,member) ))
+
+#define list_for_each_entry(pos, head, member)                                            \
+	for(pos = container_of((head)->next, __typeof__(*pos), member); &pos->member != (head); \
+		pos = container_of(pos->member.next, __typeof__(*pos), member))
+          //block, &free_list, node
 #endif
