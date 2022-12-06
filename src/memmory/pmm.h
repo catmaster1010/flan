daf178cc3f7224cc60725a4f4d0914181f47c3ae
Nich9e <66039026+nichoke@users.noreply.github.com>
2022-12-06 12:58:02 -0500

    Updated formating

--- src/memmory/pmm.h
+++ src/memmory/pmm.h
@@ -14,6 +14,7 @@ struct alloc_node {
 }__attribute__((packed));
 typedef struct alloc_node alloc_node_t;
 
+
 void dll_add( dll_t* n,  dll_t* head);
 void add_block(uint64_t *addr, uint64_t size);
 uint64_t* malloc(uint64_t size);
@@ -23,10 +24,10 @@ void free();
 
 void dll_list_add(dll_t* n, dll_t* prev, dll_t* next);
 
+
 #define ALIGN_UP(num,align) (((num) + align - 1) & ~(align - 1)) 
 #define ALIGN_DOWN(num,align) ((num) & ~(align - 1))
-#define container_of(ptr, type, member) ( \
-        (type *)( (char *)ptr - offsetof(type,member) ))
+#define container_of(ptr, type, member) ((type *)( (char *)ptr - offsetof(type,member) ))
 
 #define list_for_each_entry(pos, head, member)                                            \
 	for(pos = container_of((head)->next, __typeof__(*pos), member); &pos->member != (head); \
