8d55898f9c54a6a7e4636f6f3646076601e2a7ec
nichoke <nichoke@github.com>
2022-12-30 16:23:57 -0500

    Not finished.

--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -1,5 +1,6 @@
 #ifndef cpu_h
 #define cpu_h
+
 typedef struct __attribute__((packed)) {
     uint32_t rsvd0;
     uint64_t rsp0;
@@ -16,4 +17,24 @@ typedef struct __attribute__((packed)) {
     uint64_t rsvd2;
     uint32_t iopb;
 } tss_t;
+
+static inline uint64_t wrmsr(uint32_t msr, uint64_t val) {
+    uint32_t eax = (uint32_t)val, edx = (uint32_t)(val >> 32);
+    __asm__ volatile (
+        "wrmsr"
+        : : "a" (eax), "d" (edx), "c" (msr)
+        : "memory"
+    );
+    return ((uint64_t)edx << 32) | eax;
+}
+
+
+static inline void cpu_set_gs_base(void *addr) {
+    wrmsr(0xc0000101, (uint64_t)addr);
+}
+
+static inline void cpu_set_fs_base(void *addr) {
+    wrmsr(0xc0000100, (uint64_t)addr);
+}
+
 #endif
--- src/cpu/smp.c
+++ src/cpu/smp.c
@@ -1,10 +1,15 @@
 #include "cpu/smp.h"
+#include "cpu/cpu.h"
 #include <limine.h>
 #include <stdint.h>
 #include "memory/kheap.h"
 #include "cpu/interrupts/idt.h"
 #include "memory/gdt/gdt.h"
 #include "memory/vmm.h"
+#include "memory/pmm.h"
+
+#define CPU_STACK_SIZE 0x10000
+
 static volatile struct limine_smp_request smp_request = {
     .id = LIMINE_SMP_REQUEST,
     .revision = 0
@@ -15,11 +20,24 @@ static void  core_init(struct limine_smp_info* info){
     int cpu_number = local->cpu_number;
     local->lapic_id = info->lapic_id;
 
-
     gdt_load();
     idt_load();
     gdt_load_tss(&local->tss);
     vmm_switch_pagemap(kernel_pagemap);
+
+    cpu_set_gs_base((uint64_t)local->cpu_number);
+
+    uint64_t* int_stack_phys = pmm_calloc(CPU_STACK_SIZE / FRAME_SIZE);
+    assert(int_stack_phys);
+
+    local->tss.rsp0 = (uint64_t)((void*)int_stack_phys + CPU_STACK_SIZE + HHDM_OFFSET);
+
+    uint64_t* sched_stack_phys = pmm_calloc(CPU_STACK_SIZE / FRAME_SIZE);
+    assert(sched_stack_phys);
+    
+    uint64_t* sched_stack = (uint64_t)((void*)sched_stack_phys + CPU_STACK_SIZE + HHDM_OFFSET);
+    local->tss.ist1 = sched_stack;
+    
 }
 
 
@@ -29,7 +47,7 @@ void  smp_init(){
 
             if  (cpu->lapic_id==smp_request.response->bsp_lapic_id) {core_init(cpu);}
 
-            cpu_local_t* local = kheap_malloc( sizeof(cpu_local_t));
+            cpu_local_t* local = kheap_malloc(sizeof(cpu_local_t));
             cpu->extra_argument = (uint64_t)local;
             local->cpu_number = i;
             cpu->goto_address=core_init;
