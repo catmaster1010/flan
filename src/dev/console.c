fb836a9932895e1fe6bdee1ac2b23866f636f814
catmaster <dximixi@gmail.com>
2023-06-13 17:39:55 -0400

    fix: console_init

--- src/dev/console.c
+++ src/dev/console.c
@@ -8,7 +8,7 @@ static struct flanterm_context *term_context = NULL;
 
 void console_init(){
     struct limine_framebuffer* framebuffer = fb.response->framebuffers[0];
-    struct flanterm_context *term= flanterm_fb_init(
+    term_context =  flanterm_fb_init(
         kheap_alloc,
         kheap_free,
         framebuffer->address, framebuffer->width, framebuffer->height, framebuffer->pitch,
